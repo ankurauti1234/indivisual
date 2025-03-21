95049876c660544fb3606700eaeea514cb9f64ac
Ankur Auti <168187227+ankurauti1234@users.noreply.github.com>
2025-03-21 14:55:38 +0530

    Update auth-context.js

--- src/lib/auth-context.js
+++ src/lib/auth-context.js
@@ -5,7 +5,7 @@ import { createContext, useContext, useState, useEffect } from "react";
 const AuthContext = createContext();
 
 const dummyUsers = [
-  { email: "admin@inditronics.com", password: "Admin@123", role: "admin" },
+  // { email: "admin@inditronics.com", password: "Admin@123", role: "admin" },
   { email: "ankur.auti@inditronics.com", password: "Ankur@123", role: "user" },
   // { email: "guest@inditronics.com", password: "Guest@123", role: "guest" },
 ];
